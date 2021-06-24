af6664f14287da6ebf05f7db388d4e955fccc083
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-06-24 22:48:16 +0530

    Create Centroid.cpp

---
+++ Centroid.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=998244353;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************
+vector<int>adj[200005];
+int sz[200005];
+vector<int>centroids;
+void dfs(int src,int par,int n){
+    bool is=true;
+    sz[src]=1;
+    for(auto edge:adj[src]){
+        if(edge==par) continue;
+        dfs(edge,src,n);
+        sz[src]+=sz[edge];
+        if(sz[edge]>n/2) is=false;
+    }
+    if(n-sz[src]>n/2) is=false;
+    if(is) centroids.push_back(src);
+}
+void solve(){
+    int n;cin>>n;
+    for(int i=1;i<=n-1;i++){
+        int u,v;cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    dfs(1,-1,n);
+    cout<<centroids[0]<<'\n';
+}      
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
