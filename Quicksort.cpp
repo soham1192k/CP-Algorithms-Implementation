179230dcaeeb05f1c1c9fba0205acaf06aca8732
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-08-03 22:24:57 +0530

    Create Quicksort.cpp

---
+++ Quicksort.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************
+
+int partition(vector<int>&a,int low,int high){
+    int pivot=a[high];
+    int i=low-1;
+    for(int j=low;j<high;j++){
+        if(a[j]<pivot){
+            i++;
+            swap(a[j],a[i]);
+        }
+    }
+    swap(a[i+1],a[high]);
+    return i+1;
+}
+void quicksort(vector<int>&a,int low,int high){
+    if(low<high){
+        int pi=partition(a,low,high);
+        quicksort(a,low,pi-1);
+        quicksort(a,pi+1,high);
+    }
+}
+void solve(){
+    int n;cin>>n;
+    vector<int>a(n);
+    for(int&x:a) cin>>x;
+    quicksort(a,0,n-1);
+    for(auto x:a) cout<<x<<" ";
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
