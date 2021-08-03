d1fa66805f13d2192e1d805d57ffe923d47ff732
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-08-03 22:50:39 +0530

    Create Merge_sort_inversion_count.cpp

---
+++ Merge_sort_inversion_count.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************
+
+int cnt=0;
+void merge(vector<int>&a,int low,int mid,int high){
+    vector<int>hold(high-low+1);
+    int k=0;
+    int i=low;
+    int j=mid+1;
+    while(i<=mid&&j<=high){
+        if(a[i]<=a[j]){
+            hold[k++]=a[i++];
+        }
+        else{
+            cnt+=mid-i+1;
+            hold[k++]=a[j++];
+        }
+    }
+    while(i<=mid){
+        hold[k++]=a[i++];
+    }
+    while(j<=high){
+        hold[k++]=a[j++];
+    }
+    int cpy=low;
+    for(int i=0;i<high-low+1;i++){
+        a[cpy++]=hold[i];
+    }
+}
+void merge_sort(vector<int>&a,int low,int high){
+    if(low<high){
+        int mid=(low+high)/2;
+        merge_sort(a,low,mid);
+        merge_sort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
+}
+void solve(){
+    int n;cin>>n;
+    vector<int>a(n);
+    for(int&x:a) cin>>x;
+    merge_sort(a,0,n-1);
+    cout<<cnt<<'\n';
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
