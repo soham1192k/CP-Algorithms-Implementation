ca394db465c91384b39cff6ecd694c41a7d6b64d
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-08-04 00:40:32 +0530

    Create postorder_traversal_iterative.cpp

---
+++ postorder_traversal_iterative.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************
+
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val=val;
+        this->left=this->right=NULL;
+    }
+};
+void postorder(Node* root){
+    stack<Node*>s1,s2;
+    s1.push(root);
+    while(!s1.empty()){
+        Node* curr=s1.top();
+        s1.pop();
+        s2.push(curr);
+        if(curr->left) s1.push(curr->left);
+        if(curr->right) s1.push(curr->right);
+    }
+    while(!s2.empty()){
+        cout<<s2.top()->val<<'\n';
+        s2.pop();
+    }
+}
+void solve(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    postorder(root);
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
