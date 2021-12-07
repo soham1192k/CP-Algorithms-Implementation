7682758158fbe993d6f4b7ac1d67f1a0381c493a
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-12-07 23:59:07 +0530

    updated trie implementation

--- Trie.cpp
+++ Trie.cpp
@@ -1,106 +1,74 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long 
-#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
-#else
-#define debug(x)
-#endif
-const int mod=1e9+7;
-const double pi = 3.14159265358979323846;
-const int mxN=2e5+1;
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(double t) {cerr << t;}
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-//******************************************************************************************************************************************
-
-class TrieNode{
-public:
-    TrieNode* children[26];
-    bool isEndofWord;
-    TrieNode(){
-        for(int i=0;i<=25;i++) this->children[i]=NULL;
-        this->isEndofWord=false;
+class Node{
+    public:
+    Node* links[26];
+    bool flag;
+    int wordcount;
+    int prefixcount;
+    Node(){
+        for(int i=0;i<26;i++){
+            this->links[i]=NULL;
+        }
+        this->flag=false;
+        this->wordcount=this->prefixcount=0;
+    }
+    bool containsKey(char ch){
+        return (this->links[ch-'a']!=NULL);
+    }
+    Node* get(char ch){
+        return this->links[ch-'a'];
+    }
+    void put(char ch,Node* node){
+        this->links[ch-'a']=node;
     }
 };
-void insert(TrieNode* root,string key){
-    TrieNode* curr=root;
-    for(int i=0;i<key.length();i++){
-        int index=key[i]-'a';
-        if(curr->children[index]==NULL) curr->children[index]=new TrieNode();
-        curr=curr->children[index];
+class Trie{
+    private:
+    Node* root;
+    public:
+    Trie(){
+        root=new Node();
     }
-    curr->isEndofWord=true;
-}
-bool search(TrieNode* root,string key){
-    TrieNode* curr=root;
-    for(int i=0;i<key.length();i++){
-        int index=key[i]-'a';
-        if(curr->children[index]==NULL) return false;
-        curr=curr->children[index];
+    void insert(string& word){
+        Node* node=root;
+        for(int i=0;i<word.length();i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i],new Node());
+            }
+            node=node->get(word[i]);
+            node->prefixcount++;
+        }
+        node->wordcount++;
+        node->flag=true;
     }
-    if(curr->isEndofWord) return true;
-    return false;
-}
-bool isEmpty(TrieNode* root){
-    for(int i=0;i<26;i++){
-        if(root->children[i]) return false;
+    int countWordsEqualTo(string& word){
+        Node* node=root;
+        for(int i=0;i<word.length();i++){
+            if(!node->containsKey(word[i])){
+                return 0;
+            }
+            node=node->get(word[i]);
+        }
+        return node->wordcount;
     }
-    return true;
-}
-TrieNode* remove(TrieNode* root,string key,int depth=0){
-    if(root==NULL) return NULL;
-    if(depth==key.size()){
-        if(root->isEndofWord) root->isEndofWord=false;
-        if(isEmpty(root)) {delete(root);root=NULL;}
-        return root;
+    int countWordsStartingWith(string& prefix){
+        Node* node=root;
+        for(int i=0;i<prefix.length();i++){
+            if(!node->containsKey(prefix[i])){
+                return 0;
+            }
+            node=node->get(prefix[i]);
+        }
+        return node->prefixcount;
     }
-    int index=key[depth]-'a';
-    root->children[index]=remove(root->children[index],key,depth+1);
-    if(isEmpty(root)&&root->isEndofWord==false){
-        delete(root);
-        root=NULL;
+    void erase(string& word){
+        Node* node=root;
+        for(int i=0;i<word.size();i++){
+            if(node->containsKey(word[i])){
+                node=node->get(word[i]);
+                node->prefixcount--;
+            }
+            else return;
+        }
+        node->wordcount--;
     }
-    return root;
-}
-void solve(){
-    int n;cin>>n;
-    TrieNode* root=new TrieNode();
-    for(int i=1;i<=n;i++){
-        string s;cin>>s;
-        insert(root,s);
-    }
-    int m;cin>>m;
-    for(int i=1;i<=m;i++){
-        string s;cin>>s;
-        cout<<search(root,s)<<'\n';
-    }
-    remove(root,"heroplane",0);
-    cout<<search(root,"heroplane")<<'\n';
-}       
-int32_t main(){
-    soham1192k;
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("error.txt","w",stderr);
-#endif  
-    int T=1;
-    // cin>>T;
-    while(T--){
-        solve();
-    }
-    return 0;
-}
+};
